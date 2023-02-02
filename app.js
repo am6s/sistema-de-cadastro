5637bd73f636cf3a6ebf2dc50de07b8f9d4c1b68
am6s <amos.boca@uol.com.br>
2023-02-02 20:38:19 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+// mais um console log de teste:
 console.log("o paulo fez esse commit sem vc ver na web")
+console.log("Enviando para o GitHub")
